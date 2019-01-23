133ce948df6e173d12afa24f54d89d18e8a5e003
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-23 03:57:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
   
   def save 
     sql =  <<-SQL 
-      INSERT INTO students (name, grade) VALUES 
+      INSERT INTO students (name, grade) VALUES (?,?)
         SQL
     DB[:conn].execute(sql)
   end
