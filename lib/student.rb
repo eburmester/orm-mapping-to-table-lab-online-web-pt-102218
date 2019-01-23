2c1d1f7991437914b21b823d1e1a43411b9b7bf4
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-23 03:54:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,13 @@ class Student
   end 
   
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE IF EXISTS sqlite_master.students
-    SQL  
+    sql =  <<-SQL 
+      DROP TABLE IF EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
     DB[:conn].execute(sql)
-  end 
   
 end
